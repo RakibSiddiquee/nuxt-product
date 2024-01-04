ee8b09fe0b340a06a4dd4ef0a4226b18dc520a56
axil-rakib <rakib@axilweb.com>
2024-01-05 01:11:33 +0600

    Nuxt 3 product showcase project added

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur minima velit quibusdam modi voluptatum tenetur atque perferendis nihil doloribus iure.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur minima velit quibusdam modi voluptatum tenetur atque perferendis nihil doloribus iure.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <ProductCard :product="p" />
+            </div>
+        </div>
+        <NuxtLink to="products/123">Details Product</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'products'
+    })
+
+    console.log('dd')
+
+    // fetch the products
+    const {data: products} = await useFetch('https://fakestoreapi.com/products')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
